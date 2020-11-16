03d59503ab094b721682a8ecf7113ca065d0e8a7
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 10:19:40 +0100

    FIX: Refactored flats controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,14 +1,18 @@
 require "open-uri"
 
 class FlatsController < ApplicationController
-  def home
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
-  end
+  FLATS_URL = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+  before_action :set_flats
+
+  def home; end
 
   def show
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    flats = JSON.parse(open(url).read)
-    @flat = flats.find { |flat| flat['id'] == params['id'].to_i }
+    @flat = @flats.find { |flat| flat['id'] == params['id'].to_i }
+  end
+
+  private
+
+  def set_flats
+    @flats = JSON.parse(open(FLATS_URL).read)
   end
 end
