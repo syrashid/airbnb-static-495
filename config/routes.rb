5b199a6229875c0ec34c2578dcf9d8dfb27c80e9
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 10:06:13 +0100

    FEAT: Added dynamic show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html'
   root 'flats#home'
+  # verb  url   to: controller#action
+  get 'flats/:id', to: 'flats#show', as: :flat
 end
