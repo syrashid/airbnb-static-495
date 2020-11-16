6b9d8d7904f5592a34b832e4f8cc59167e3123a2
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 09:47:22 +0100

    FEAT: Finished index action, wrote the second test

--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -6,4 +6,10 @@ class FlatsTest < ApplicationSystemTestCase
 
     assert_selector 'h1', text: "Our Home Page with Flats"
   end
+
+  test "the user visits a dynamic show page and sees info on the flat" do
+    visit 'flats/145'
+
+    assert_selector 'h1', text: "Charm at the Steps of the Sacre Coeur/Montmartre"
+  end
 end
